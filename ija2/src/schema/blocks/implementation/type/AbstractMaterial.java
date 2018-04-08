19ac21d98d1bf9933b469c317188074bb1ef9ab8
xnesme00 <oslikna@gmail.com>
2018-04-08 19:50:33 +0200

    Add control of state

--- ija2/src/schema/blocks/implementation/type/AbstractMaterial.java
+++ ija2/src/schema/blocks/implementation/type/AbstractMaterial.java
@@ -6,12 +6,29 @@ import schema.blocks.Type;
 public abstract class AbstractMaterial implements Material {
 	public double kg;
 	public double t;
-	public String state;
+	public AbstractMaterial.State state;
 	public double thermal_c;
 	public double freeze_t;
 	public double gass_t;
 	public double gass_const;
 	public double melt_const;
+
+	public enum State {
+		GASS,
+		LIQUID,
+		ICE;
+
+		@Override
+		public String toString() {
+			switch (this) {
+				case GASS:    return "gass";
+				case LIQUID: return "liquid";
+				case ICE:   return "ice";
+			}
+
+			throw new AssertionError("Unknown type: " + this);
+		}
+	}
 
 	public AbstractMaterial() {
 
@@ -20,6 +37,28 @@ public abstract class AbstractMaterial implements Material {
 	public void setValues(double kg, double t) {
 		this.kg = kg;
 		this.t = t;
+		this.controlState();
+	}
+
+	public double getMass() {
+		return this.kg;
+	}
+
+	public double getTemp() {
+		return this.t;
+	}
+
+	public AbstractMaterial.State getState() {
+		return this.state;
+	}
+
+	public void controlState() {
+		if (this.t < this.freeze_t)
+			this.state = State.ICE;
+		else if (this.t < this.gass_t)
+			this.state = State.LIQUID;
+		else
+			this.state = State.GASS;
 	}
 
 	@Override
