40af009c167687d28a309d9d1e8710907d2ddca9
Pirkl Nina <nina.pirkl3@gmail.com>
2018-04-24 14:01:24 +0200

    Update Window.java

--- ija2/src/schema/window/Window.java
+++ ija2/src/schema/window/Window.java
@@ -1,76 +1,36 @@
 package schema.window;
 
-import java.awt.EventQueue;
+import javax.imageio.ImageIO;
+import javax.swing.*;
 
-import javax.swing.JFrame;
-import javax.swing.JButton;
-import javax.swing.SwingConstants;
-import java.awt.event.ActionListener;
+import schema.window.Panel;
+
+import java.awt.*;
 import java.awt.event.ActionEvent;
-import javax.swing.JDesktopPane;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Window {
 
-	private JFrame frame;
+    public static String NAME = "Block Schema's editor";     //name of window
+    public static boolean splitted;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Window window = new Window();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    public static void main(String[] args) {
 
-	/**
-	 * Create the application.
-	 */
-	public Window() {
-		initialize();
-	}
+        JFrame frame = new JFrame(NAME);
+        splitted = false;
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 663, 435);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
-		
-		JButton btnNewButton = new JButton("Warm");
-		btnNewButton.setBounds(12, 72, 83, 39);
-		frame.getContentPane().add(btnNewButton);
-		
-		JButton btnNewButton_1 = new JButton("Freeze");
-		btnNewButton_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		btnNewButton_1.setBounds(12, 133, 83, 39);
-		frame.getContentPane().add(btnNewButton_1);
-		
-		JButton btnMakeIce = new JButton("Make Ice");
-		btnMakeIce.setBounds(10, 192, 85, 39);
-		frame.getContentPane().add(btnMakeIce);
-		
-		JButton btnMakeLiquid = new JButton("Make liquid");
-		btnMakeLiquid.setBounds(10, 253, 85, 39);
-		frame.getContentPane().add(btnMakeLiquid);
-		
-		JButton btnMakeGas = new JButton("Make gas");
-		btnMakeGas.setBounds(12, 314, 85, 39);
-		frame.getContentPane().add(btnMakeGas);
-		
-		JDesktopPane desktopPane = new JDesktopPane();
-		desktopPane.setBounds(114, 49, 516, 320);
-		frame.getContentPane().add(desktopPane);
-	}
+        frame.getContentPane().setLayout(new GridBagLayout());
+        frame.getContentPane().add(new Panel(frame));
+
+
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setResizable(false);
+    }
 }
