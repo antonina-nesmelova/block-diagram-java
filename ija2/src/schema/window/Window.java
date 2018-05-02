41832d4526828cb0a972ae32598070ebf3bbe22a
Pirkl Nina <nina.pirkl3@gmail.com>
2018-05-02 14:00:53 +0200

    Update Window.java

--- ija2/src/schema/window/Window.java
+++ ija2/src/schema/window/Window.java
@@ -5,15 +5,21 @@ import java.awt.event.*;
 import java.util.Random;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
+
+import schema.Schema;
+import schema.blocks.implementation.blocks.Block;
+import schema.blocks.implementation.blocks.Block.Operation;
 
 import static interfaces.Constants.*;
 
 public class Window {
-
+	public Schema schema;
     private JFrame frame;
     public static String NAME = "Block diagrams modeling physical processes";
-    private JDesktopPane desktopPane;
+    protected static JDesktopPane desktopPane;
+    private int number;
 	
     /**
      * Launch the application.
@@ -36,113 +42,11 @@ public class Window {
      * Create the application.
      */
     public Window() {
+    	this.number = 0;
         initialize();
-    }
-
-    public JPanel Data() {      	
-    	JPanel panel = new JPanel();
-		panel.setBounds(0, 0, 125, 86);
-		panel.setBackground(new Color(245, 222, 179));
-		desktopPane.add(panel);
-		panel.setLayout(null);
-		
-        JFormattedTextField MassField = new JFormattedTextField();
-        MassField.setToolTipText("Mass");
-        //MassField.setValue(0);
-        MassField.setBounds(14, 40, 85, 20);
-        panel.add(MassField);
-        frame.setVisible(true);
-        
-        MassField.addActionListener(new ActionListener() {
-    	    @Override
-    	    public void actionPerformed(ActionEvent event) {
-    	        System.out.println("The entered text is: " + MassField.getText());
-    	    }
-    	});
-        
-        JFormattedTextField TemperatureField = new JFormattedTextField();
-        TemperatureField.setToolTipText("Temperature");
-        TemperatureField.setValue(MassField.getValue());
-        TemperatureField.setBounds(14, 60, 85, 20);
-        panel.add(TemperatureField);
-        frame.setVisible(true); 
-        
-        TemperatureField.setValue(MassField.getText());
-        TemperatureField.addActionListener(new ActionListener() {
-    	    @Override
-    	    public void actionPerformed(ActionEvent event) {
-    	        System.out.println("The entered text is: " + MassField.getText());
-    	    }
-    	});
-		return panel;
+        Schema schema = new Schema();
     }
     
-    private void block_creator(int t, int x, int y) {   
-		JPanel block = new JPanel();
-		block.setBackground(new Color(255, 250, 240));
-        block.setBounds(x, y, BLOCK_WIDTH, BLOCK_HEIGHT);
-        desktopPane.add(block);
-        frame.setVisible(true);
-        JLabel jlabel = null;
-            
-    		if(t == 0) {  
-                jlabel = new JLabel("Warm");   
-    		}
-    		else if(t == 1) {
-                jlabel = new JLabel("Freeze"); 
-    		}
-    		else if(t == 2) {
-                jlabel = new JLabel("Make Ice"); 
-    		}
-    		else if(t == 3) {
-                jlabel = new JLabel("Make Liquid");
-    		}
-    		else if(t == 4) {
-                jlabel = new JLabel("Make Gas"); 
-    		}
-    		
-    	/*	Name of the block	*/
-    	jlabel.setFont(new Font("Verdana", 1, 14));
-        block.add(jlabel, BorderLayout.NORTH);
-        jlabel.setBounds(28, 15, 85, 25);
-        block.setBorder(new LineBorder(Color.BLACK));
-        
-        /*	Choice Type */
-        Choice choice_1 = new Choice();
-        block.add(choice_1, BorderLayout.CENTER);
-        choice_1.setBounds(25, 50, 85, 25);
-        choice_1.add("Water");
-        choice_1.add("Alkohol");
-        choice_1.add("Energy");
-        
-        /*	Delete Button */
-        JButton delete = new JButton("Delete");
-        delete.setToolTipText("Delete Block");
-        block.setLayout(new BorderLayout());
-        block.add(delete, BorderLayout.SOUTH);
-        frame.setVisible(true);
-        
-        /*	Mass Button */
-        /*JButton mass = new JButton("+");
-        mass.setToolTipText("Insert Mass");
-        block.add(mass);
-        frame.setVisible(true);
-        mass.addActionListener(new ActionListener() {
-    		public void actionPerformed(ActionEvent arg0) {
-    			JPanel panel = Data();
-    		}
-        });	*/	
-    	delete.addActionListener(new ActionListener() {
-    		public void actionPerformed(ActionEvent arg0) {
-    			desktopPane.remove(block);
-            	desktopPane.revalidate();
-            	desktopPane.repaint();
-            }
-    	});
-    	
-
-      Movement drag = new Movement(block);
- }
     /**
      * Initialize the contents of the frame.
      */
@@ -166,10 +70,16 @@ public class Window {
     	btnWarm.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent arg0) {
             	int x = rand.nextInt(SCREEN_WIDTH - 500) + 100;
-        		int y = rand.nextInt(SCREEN_HEIGHT - 500) + 100;
-            	block_creator(0, x, y);
+    			int y = rand.nextInt(SCREEN_HEIGHT - 500) + 100;
+    			Block logicBlock = schema.createBlock(Operation.WARM);
+            	BlockShape block = new BlockShape(0, x, y, number, logicBlock);
+            	number += 1;
+                desktopPane.add(block.block_creator(0, x, y));
+                frame.setVisible(true);
         	}
         });
+    	
+    	
         btnWarm.setForeground(new Color(128, 0, 128));
         btnWarm.setFont(new Font("Source Code Pro Semibold", Font.PLAIN, 12));
         btnWarm.setBackground(new Color(211, 211, 211));
@@ -182,8 +92,12 @@ public class Window {
             public void actionPerformed(ActionEvent arg0) {     	
             	int x = rand.nextInt(SCREEN_WIDTH - 500) + 100;
         		int y = rand.nextInt(SCREEN_HEIGHT - 500) + 100;
-            	block_creator(1, x, y);
-            }
+        		Block logicBlock = schema.createBlock(Operation.FREEZE);
+        		BlockShape block = new BlockShape(1, x, y, number, logicBlock);
+            	number += 1;
+                desktopPane.add(block.block_creator(1, x, y));
+                frame.setVisible(true);
+        	}
         });
         btnFreeze.setForeground(new Color(128, 0, 128));
         btnFreeze.setFont(new Font("Source Code Pro Semibold", Font.PLAIN, 12));
@@ -197,7 +111,11 @@ public class Window {
             public void actionPerformed(ActionEvent arg0) {     	
             	int x = rand.nextInt(SCREEN_WIDTH - 500) + 100;
         		int y = rand.nextInt(SCREEN_HEIGHT - 500) + 100;
-            	block_creator(2, x, y);
+        		Block logicBlock = schema.createBlock(Operation.MKICE);
+        		BlockShape block = new BlockShape(2, x, y, number, logicBlock);
+            	number += 1;
+                desktopPane.add(block.block_creator(2, x, y));
+                frame.setVisible(true);       		
             }
         });
         btnMakeIce.setForeground(new Color(128, 0, 128));
@@ -212,8 +130,12 @@ public class Window {
             public void actionPerformed(ActionEvent arg0) {     	
             	int x = rand.nextInt(SCREEN_WIDTH - 500) + 100;
         		int y = rand.nextInt(SCREEN_HEIGHT - 500) + 100;
-            	block_creator(3, x, y);
-            }
+        		Block logicBlock = schema.createBlock(Operation.MKLIQUID);
+        		BlockShape block = new BlockShape(3, x, y, number, logicBlock);
+            	number += 1;
+                desktopPane.add(block.block_creator(3, x, y));
+                frame.setVisible(true);
+        	}
         });
         btnMakeLiquid.setForeground(new Color(128, 0, 128));
         btnMakeLiquid.setFont(new Font("Source Code Pro Semibold", Font.PLAIN, 12));
@@ -226,8 +148,12 @@ public class Window {
             public void actionPerformed(ActionEvent arg0) {     	
             	int x = rand.nextInt(SCREEN_WIDTH - 500) + 100;
         		int y = rand.nextInt(SCREEN_HEIGHT - 500) + 100;
-            	block_creator(4, x, y);
-            }
+        		Block logicBlock = schema.createBlock(Operation.MKGASS);
+        		BlockShape block = new BlockShape(4, x, y, number, logicBlock);
+            	number += 1;
+                desktopPane.add(block.block_creator(4, x, y));
+                frame.setVisible(true);
+        	}
         });
         btnMakeGas.setForeground(new Color(128, 0, 128));
         btnMakeGas.setFont(new Font("Source Code Pro Semibold", Font.PLAIN, 12));
