f98aca053fd5d761ad640128e0fed0126c3990ce
Pirkl Nina <nina.pirkl3@gmail.com>
2018-05-03 16:41:08 +0200

    Create Choice.java

---
+++ ija2/src/schema/window/Choice.java
@@ -0,0 +1,23 @@
+package schema.window;
+
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+import javax.swing.JComboBox;
+import javax.swing.JPanel;
+
+public class Choice extends JPanel implements ItemListener {
+	  public Choice(JPanel panel) {
+		  JComboBox type_choice = new JComboBox(); 
+	      panel.add(type_choice);
+	      type_choice.setBounds(13, 12, 70, 22);
+	      type_choice.addItem("Water"); 
+	      type_choice.addItem("Alkohol"); 
+	      type_choice.addItem("Energy"); 
+	      type_choice.addItemListener((ItemListener) this); 
+	  }
+
+	  public void itemStateChanged(ItemEvent ie) {
+	      BlockShape.sel_ch = (String)ie.getItem();
+	  }    
+}
